fab93b879152ffe8bd67123ef31c69ce8a4d93f6
Bahaeeddine15 <b.mekrane9303@uca.ac.ma>
2025-08-15 22:12:05 +0100

    article fixed

--- app/Models/formation.php
+++ app/Models/formation.php
@@ -2,18 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Formation extends Model // Majuscule pour Formation
+class Formation extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
-        'titre',
+        'title',
         'description',
+        'level',
+        'duration',
         'category',
-        'niveau',
-        'photo'
+        'link',
+        'thumbnail'
     ];
+
+    public function modules()
+    {
+        return $this->hasMany(Module::class);
+    }
 }
