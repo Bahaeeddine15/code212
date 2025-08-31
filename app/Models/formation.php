e2df5aa6a8238be2a4adacc5f5c24fb08737e194
mouadaeh <m.aitelhachmi7790@uca.ac.ma>
2025-08-31 15:07:58 +0100

    feat: Add registration relationship and status scopes to Formation model

--- app/Models/formation.php
+++ app/Models/formation.php
@@ -20,20 +20,4 @@ class Formation extends Model
     {
         return $this->hasMany(Module::class);
     }
-
-
-    public function registrations()
-    {
-        return $this->hasMany(\App\Models\FormationRegistration::class, 'formation_id');
-    }
-
-    public function scopePublished($query)
-    {
-        return $query->where('status', 'published');
-    }
-
-    public function scopeDrafts($query)
-    {
-        return $query->where('status', 'draft');
-    }
 }
